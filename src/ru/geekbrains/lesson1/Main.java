bb660f07c2dc00ff00259c7a986f5550d983ba52
pr0brol <pr0_brol@mail.ru>
2019-07-16 13:54:12 +0300

    homework-1

---
+++ src/ru/geekbrains/lesson1/Main.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.lesson1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        while (true){
+            if(N < 2 || N > 10000){
+                System.out.println("NO");
+                break;
+            }else if(N == 2){
+                System.out.println("YES");
+                break;
+            }else {
+                if(N % 2 != 0){
+                    System.out.println("NO");
+                    break;
+                }
+                N /= 2;
+                System.out.print(N + " ");
+            }
+        }
+    }
+}
